cb5fbef8479d43de457adf6d6ea3b4d81fd08fba
Maximilian Herzog <maxi.herzog@t-online.de>
2020-04-01 00:25:32 +0200

    Cleaned up code with flake8 * Some bugfixes * Removed del ... * TODO: Remove bare 'except's

--- grid_infra_score_gen.py
+++ grid_infra_score_gen.py
@@ -1,30 +1,23 @@
- #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 21 15:28:56 2020
-@author: maxi
-"""
-
 import pandas as pd
-pd.set_option('display.max_rows', 20)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
 import geopandas as gpd
 from geopandas import GeoDataFrame
 from shapely.geometry import Point
-from shapely.geometry import Polygon
+# from shapely.geometry import Polygon
 import numpy as np
 import overpy
 import sys
 import time
 
-### Used for displaying Progressbars
-# Shoutout to eusoubrasileiro on Stackoverflow!
+
 def progressbar(it, prefix="", size=60, file=sys.stdout):
+    # Used for displaying Progressbars
+    # Shoutout to eusoubrasileiro on Stackoverflow!
     count = len(it)
+
     def show(j):
         x = int(size*j/count)
-        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j,
+                   count))
         file.flush()
     show(0)
     for i, item in enumerate(it):
@@ -33,28 +26,38 @@ def progressbar(it, prefix="", size=60, file=sys.stdout):
     file.write("\n")
     file.flush()
 
-#Main method of generator
+
 def main(args):
+    # Main method of generator
     if not (len(args) == 4 or len(args) == 7):
         print("Missing arguments. Usage:")
-        print("python3 grid_infra_score_gen.py GRID.shp TAG_WEIGHTS.csv INFRA_SCORES.shp (BLUR_RADIUS GRID_WIDTH GRID_HEIGHT)")
-        print("(BLUR RADIUS GRID_WIDTH GRID_HEIGHT) is optional, if you want to blur the outcoming infrastructure values over the grid.")
+        print("python3 grid_infra_score_gen.py GRID.shp TAG_WEIGHTS.csv "
+              "INFRA_SCORES.shp (BLUR_RADIUS GRID_WIDTH GRID_HEIGHT)")
+        print("(BLUR RADIUS GRID_WIDTH GRID_HEIGHT) is optional, if you want "
+              "to blur the outcoming infrastructure values over the grid.")
         return
     if len(args) == 7:
-        try:
+        if isinstance(args[5], (int, float)):
             blur_radius = float(args[4])
-        except:
+        else:
             print("Invalid blur radius. Input a float! Usage: ")
-            print("python3 grid_infra_score_gen.py GRID.shp TAG_WEIGHTS.csv INFRA_SCORES.shp (BLUR_RADIUS GRID_WIDTH GRID_HEIGHT)")
-            print("(BLUR RADIUS GRID_WIDTH GRID_HEIGHT) is optional, if you want to blur the outcoming infrastructure values over the grid.")
+            print("python3 grid_infra_score_gen.py GRID.shp TAG_WEIGHTS.csv"
+                  "INFRA_SCORES.shp (BLUR_RADIUS GRID_WIDTH GRID_HEIGHT)")
+            print("(BLUR RADIUS GRID_WIDTH GRID_HEIGHT) is optional, if you "
+                  "want to blur the outcoming infrastructure values over the "
+                  "grid.")
             return
-        try:
+        if isinstance(args[5], int) and isinstance(args[6], int):
             grid_width = int(args[5])
             grid_height = int(args[6])
-        except:
-            print("Invalid grid dimensions GRID_WIDTH and GRID_HEIGHT. Input Integers! Usage:")
-            print("python3 grid_infra_score_gen.py GRID.shp TAG_WEIGHTS.csv INFRA_SCORES.shp (BLUR_RADIUS GRID_WIDTH GRID_HEIGHT)")
-            print("(BLUR RADIUS GRID_WIDTH GRID_HEIGHT) is optional, if you want to blur the outcoming infrastructure values over the grid.")
+        else:
+            print("Invalid grid dimensions GRID_WIDTH and GRID_HEIGHT. Input"
+                  " Integers! Usage:")
+            print("python3 grid_infra_score_gen.py GRID.shp TAG_WEIGHTS.csv "
+                  "INFRA_SCORES.shp (BLUR_RADIUS GRID_WIDTH GRID_HEIGHT)")
+            print("(BLUR RADIUS GRID_WIDTH GRID_HEIGHT) is optional, if you"
+                  " want to blur the outcoming infrastructure values over the "
+                  "grid.")
             return
 
         if blur_radius <= 0 or blur_radius >= 1:
@@ -68,79 +71,73 @@ def main(args):
     start_time = time.time()
 
     print("Loading Grid Shape File at " + args[1])
-
     try:
         grid = gpd.read_file(args[1])
         minx, miny, maxx, maxy = grid.geometry.total_bounds
         rnd = 4
-        bb = str(round(miny,rnd)) + "," + str(round(minx,rnd)) + "," + str(round(maxy,rnd)) + "," + str(round(maxx,rnd))
+        bb = str(round(miny, rnd)) + "," \
+            + str(round(minx, rnd)) + "," \
+            + str(round(maxy, rnd)) + "," \
+            + str(round(maxx, rnd))
         print("Found boundary box (", bb,  ")")
-        try:
-            del grid['left']
-            del grid['top']
-            del grid['bottom']
-            del grid['right']
-        except:
-            pass
     except:
-        print("Grid shape file could not be located or is in the wrong format. Do you also have the .shx and .dbf files in the same directory?. Aborting..")
+        print("Grid shape file could not be located or is in the wrong format."
+              " Do you also have the .shx and .dbf files in the same "
+              "directory? Aborting..")
         return
-
-    if not grid_width*grid_height==len(grid['geometry']):
-        print("Given grid dimensons do not match given grid file.")
-        print(str(grid_width)+"*"+str(grid_height)+"="+str(grid_width*grid_height)+"≠"+str(len(grid["geometry"])))
-        print("Aborting..")
-        return
+    if len(args) == 7:
+        if not grid_width*grid_height == len(grid['geometry']):
+            print("Given grid dimensons do not match given grid file.")
+            print(str(grid_width)+"*"+str(grid_height) + "="
+                  + str(grid_width*grid_height)+"≠"+str(len(grid["geometry"])))
+            print("Aborting..")
+            return
 
     print("Loading Tagweights at " + args[2])
     try:
-        df=pd.read_csv(args[2], sep=':',header=0)
+        df = pd.read_csv(args[2], sep=':', header=0)
     except:
         print("Tagweights file could not be located. Aborting..")
         return
 
-
     # Hier Timeout ändern, falls keine Rückgabe!
-    #resultstring = "[out:json][timeout:200];area[name=\"Heidelberg\"]->.searchArea;\n("
-
-
     resultstring = "[out:json][timeout:1000];\n("
-    crs = {'init': 'epsg:4326'} #4326
-    #df=pd.read_csv('tagweights.csv', sep=':',header=0)
+    crs = {'init': 'epsg:4326'}  # 4326
     dict = {}
     print("Collecting dict: ")
     n_cols = len(df.columns)-3
-    for i in range(0,len(df.values)):
+    for i in range(0, len(df.values)-58):
         for x in range(2, n_cols+2):
             string = str(df.values[i][x])
             if not string == "nan":
                 liststring = string.split(", ")
                 for value in liststring:
                     dict[value] = df.values[i][n_cols+2]
-                    print(df.columns[x] + ":" + value + " [" + str(df.values[i][n_cols+2]) + "]")
+                    print(df.columns[x] + ":" + value + " ["
+                          + str(df.values[i][n_cols+2]) + "]")
                     for pre in ["node", "way"]:
-                        resultstring += "\t" + pre + '[\"' + df.columns[x] + '\"=\"' + value + '\"](' + bb +');'
+                        resultstring += "\t" + pre + '[\"' + df.columns[x] \
+                            + '\"=\"' + value + '\"](' + bb + ');'
 
     resultstring += ");out; >; out;"
-    #print(resultstring)
+
     api = overpy.Overpass()
-    #print(dict["cafe"])
     print("Starting query. This may take a while(<1000s)...")
     try:
         result = api.query(resultstring)
     except:
-        print("Query failed. Either there is no internet connection or the query timed out. Aborting..")
+        print("Query failed. Either there is no internet connection or the "
+              "query timed out. Aborting..")
         return
 
-
     print("Finished query!")
-    print(str(len(result.nodes)) + " nodes found.(also containing the unzipped ways)")
-    print(str(len(result.ways )) + " ways found.")
+    print(str(len(result.nodes)) + " nodes found."
+          "(also containing the unzipped ways)")
+    print(str(len(result.ways)) + " ways found.")
 
     if len(result.nodes) == 0 and len(result.ways) == 0:
         print("Query returned no results. Maybe it took to long. Aborting...")
         return
-    #print(dict['bakery'])
 
     def findWeight(tags):
         for poss_key in df.columns[2:5]:
@@ -149,16 +146,16 @@ def main(args):
                     return dict[tags[poss_key]]
         return 0
 
-
     COLNAMES = ["point_id", "lon", "lat", "infra_score"]
-    nodedf = pd.DataFrame(columns = COLNAMES)
+    nodedf = pd.DataFrame(columns=COLNAMES)
 
-    #### F+r Fortschrittsleiste
+    # Progressbar
     # Try to get Terminal width
-    try:
-        _, terminal_columns = os.popen('stty size', 'r').read().split()
-    except:
-        terminal_columns = 60
+    # try:
+    #     _, terminal_columns = os.popen('stty size', 'r').read().split()
+    #     terminal_columns = int(terminal_columns)
+    # except:
+    terminal_columns = 60
     print("Analyzing nodes...")
     # Nodes
     nodes = result.get_nodes()
@@ -166,17 +163,15 @@ def main(args):
     for i in progressbar(range(len(nodes)), size=terminal_columns):
 
         n = nodes[i]
-        #print(n.tags)
+        # print(n.tags)
         weight = findWeight(n.tags)
         nodedf.loc[ct] = [n.id, n.lon, n.lat, weight]
-        ct+=1
-        #print(n.id, n.lon, n.lat, weight)
+        ct += 1
 
-    #Berechnung der Infrastrukturwerte der ways
+    # Berechnung der Infrastrukturwerte der ways
     print("Analyzing ways(e.g. building outlines)...")
-    #ct = len(nodedf.loc)-1
     ways = result.get_ways()
-    for i in progressbar(range(len(ways)),size=terminal_columns):
+    for i in progressbar(range(len(ways)), size=terminal_columns):
         w = ways[i]
 
         nodes = w.get_nodes(resolve_missing=False)
@@ -185,9 +180,7 @@ def main(args):
         for j in range(N):
             n = nodes[j]
             nodedf.loc[ct] = [n.id, n.lon, n.lat, weight]
-            ct+=1
-
-    #print(df)
+            ct += 1
 
     geometry = [Point(xy) for xy in zip(nodedf.lon, nodedf.lat)]
 
@@ -196,22 +189,22 @@ def main(args):
     del geo_df['lon']
     del geo_df['point_id']
 
-    #print("Eingangs- Grid: ")
-    #print(grid)
+    # print("Eingangs- Grid: ")
+    # print(grid)
 
     dfsjoin = gpd.sjoin(grid, geo_df, how="left", op='contains')
-    dfpivot = pd.pivot_table(dfsjoin, index="id", aggfunc={'infra_score': np.sum})
+    dfpivot = pd.pivot_table(dfsjoin, index="id",
+                             aggfunc={'infra_score': np.sum})
     # dfpivot.columns = dfpivot.columns.droplevel()
 
-    #print(dfpivot)
+    # print(dfpivot)
 
     dfgridnew = grid.merge(dfpivot, how='left', on="id")
-    #dfgridnew.drop(["left", "top", "right", "bottom"], axis=1)
-    #print("Gemergtes Grid: ")
-    #print(dfgridnew)
+    # dfgridnew.drop(["left", "top", "right", "bottom"], axis=1)
+    # print("Gemergtes Grid: ")
+    # print(dfgridnew)
 
-    #id, infra_wert, geometry
-
+    # Target values: id, infra_wert, geometry
 
     # ways = result.get_ways()
     # weight_list = [0] * len(ways) * 200
@@ -226,36 +219,37 @@ def main(args):
     #     lat_list = [0] * N
     #     lon_list = [0] * N
 
-        # Scheint nicht zu funktionieren -> siehe Discord
-        # for j in range(N):
-        #     n = nodes[j]
-        #     lat_list[j] = n.lat
-        #     lon_list[j] = n.lon
-        #
-        #
-        #     #print(n.id, n.lon, n.lat,weight)
-        # try:
-        #     poly_geo = Polygon(zip(lon_list, lat_list))
-        #     meta_geo[ct] = poly_geo
-        #     weight_list[ct] = findWeight(w.tags)
-        #     ct+=1
-        # except:
-        #     print("WARNING: Invalid way-shape fount @id=", w.id, "i=", i , " ignoring...")
-        #     pass
+    # Scheint nicht zu funktionieren -> siehe Discord
+    # for j in range(N):
+    #     n = nodes[j]
+    #     lat_list[j] = n.lat
+    #     lon_list[j] = n.lon
+    #
+    #
+    #     #print(n.id, n.lon, n.lat,weight)
+    # try:
+    #     poly_geo = Polygon(zip(lon_list, lat_list))
+    #     meta_geo[ct] = poly_geo
+    #     weight_list[ct] = findWeight(w.tags)
+    #     ct+=1
+    # except:
+    #     print("WARNING: Invalid way-shape fount @id=", w.id, "i=", i,
+    #           " ignoring...")
+    #     pass
 
-        #Alternative Eckpunkte mit Bewertung/Anzahl der Eckpunkte
-        # weight_w = findWeight(w.tags)
-        # for j in range(N):
-        #     n = nodes[j]
-        #     lat_list[j] = n.lat
-        #     lon_list[j] = n.lon
-        #
-        #     point_geo = Point(n.lat, n.lon)
-        #     meta_geo[ct] = point_geo
-        #     weight_list[ct] = weight_w/N
-        #     ct+=1
+    # Alternative Eckpunkte mit Bewertung/Anzahl der Eckpunkte
+    # weight_w = findWeight(w.tags)
+    # for j in range(N):
+    #     n = nodes[j]
+    #     lat_list[j] = n.lat
+    #     lon_list[j] = n.lon
+    #
+    #     point_geo = Point(n.lat, n.lon)
+    #     meta_geo[ct] = point_geo
+    #     weight_list[ct] = weight_w/N
+    #     ct+=1
 
-    #arrs zuschneiden
+    # cut arrays
     # weight_list = weight_list[:ct]
     # meta_geo = meta_geo[:ct]
     #
@@ -266,19 +260,20 @@ def main(args):
     # # print(weight_df)
     #
     # polysjoin = gpd.sjoin(grid, poly_df, how="left", op='contains')
-    # polypivot = pd.pivot_table(polysjoin, index="id", aggfunc={'infra_score': np.sum})
+    # polypivot = pd.pivot_table(polysjoin, index="id",
+    #                            aggfunc={'infra_score': np.sum})
     # # dfpivot.columns = dfpivot.columns.droplevel()
     #
     # finalgrid = grid.merge(polypivot, how='left', on="id")
     # #print(finalgrid)
     # insc_ways = np.array(finalgrid["infra_score"])
 
-    #BLUR
-    if len(args) == 5:
+    # BLUR
+    if len(args) == 7:
 
         # Get Grid width and height
         N = len(dfgridnew["geometry"])
-        #TODO: Width und Height Findung richtig implementieren
+        # TODO: Width und Height Findung richtig implementieren
 
         # Die Lösung hier funktioniert nicht, da die Gridrechtecke da neben-
         # einanderliegende Kästchen in x-richtung andere y-Werte haben(wegen
@@ -296,54 +291,46 @@ def main(args):
 
         # height = np.floor(N/weight)
 
-        # Die Lösung hier ist sehr WIP und im Allgemeinen falsch
-        #, da nicht alle grids quadratisch sind, aber in unserem Fall funtkioniert
-        # sie
-        #width = int(np.floor(np.sqrt(N)))
-        #height= int( np.ceil(np.sqrt(N)))
+        # Die Lösung hier ist sehr WIP und im Allgemeinen falsch,
+        # da nicht alle grids quadratisch sind,
+        # aber in unserem Fall funtkioniert sie
+        # width = int(np.floor(np.sqrt(N)))
+        # height= int( np.ceil(np.sqrt(N)))
 
         # Mögliche Lösungen:
         # I  : Der user gibt width und height an
-        # II : Es gibt einen intelligenteren Weg width und height herauszufinden
+        # II : Es gibt einen intelligenteren Weg width und height zu bestimmen
         # III: Es gibt einen intelligenteren Weg zu bluren
 
         # Lösung I
         width = grid_width
         height = grid_height
-        print("Found grid: (width:", width ,"height:", height,")")
+        print("Found grid: (width:", width, "height:", height, ")")
         new_infra = np.array(dfgridnew["infra_score"])
         print("Bluring the grid with blur radius", blur_radius)
         for i in progressbar(range(N)):
             blur_val = dfgridnew["infra_score"][i]*blur_radius
-            new_infra[j+1] +=  blur_val
-            new_infra[j-1] +=  blur_val
-            new_infra[j+width] +=  blur_val
-            new_infra[j-width] +=  blur_val
+            new_infra[j+1] += blur_val
+            new_infra[j-1] += blur_val
+            new_infra[j+width] += blur_val
+            new_infra[j-width] += blur_val
 
-        #apply blur
+        # Apply blur
         dfgridnew["infra_score"] = new_infra
 
-
-
+    # Final calculations
     insc = - np.array(dfgridnew["infra_score"])
-    #print(insc_nodes)
     ct = 0
     for x in insc:
-        if x==0:
-            ct+=1
-    print("Amount of unaffected cells: " + str(ct) + "/" + str(len(grid)) +
-            "  ( " + str(round(ct/len(grid)*100,2)) + "% )")
+        if x == 0:
+            ct += 1
+    print("Amount of unaffected cells: " + str(ct) + "/" + str(len(grid))
+          + "  ( " + str(round(ct/len(grid)*100, 2)) + "% )")
     print("Distribution")
-    # print("Nodes:")
     print("\tµ = ", np.mean(insc))
-    print("\tσ = ", np.std (insc))
-    # print("Ways:")
-    # print("\tµ = ", np.mean(insc_ways))
-    # print("\tσ = ", np.std (insc_ways))
-    #normalisieren
+    print("\tσ = ", np.std(insc))
 
-
-    #Normalisieren
+    # Normalization
     '''--Problem mit Wurzeln: Ich hasse Wurzeln
     fac = 0.1
     sgn = np.sign(insc)
@@ -355,25 +342,27 @@ def main(args):
     insc = insc/np.max(np.abs(insc)) + 1
     dfgridnew["infra_score"] =  insc'''
 
-    #Idee: Log. skalieren
+    # Idee: Log. skalieren
     insc = insc + abs(np.min(insc)) + 1
     insc = np.log(insc) + 1
-    dfgridnew["infra_score"] =  insc
+    dfgridnew["infra_score"] = insc
 
-    #Nochmal ausgeben
-    print("After applying a normilization function:" )
+    # Nochmal ausgeben
+    print("After applying a normilization function:")
     print("\tµ = ", np.mean(insc))
-    print("\tσ = ", np.std (insc))
-    #print("Gebäudeumrisse mit Infrastrukturwert: ")
-    #print(poly)
+    print("\tσ = ", np.std(insc))
+
+    # print("Gebäudeumrisse mit Infrastrukturwert: ")
+    # print(poly)
 
     print("Generated grid:")
     print(dfgridnew)
     dfgridnew.to_file(driver="ESRI Shapefile", filename=args[3])
     print("Saved grid to file: " + args[3])
     print("Finished")
-    print("--- "+ str(round((time.time() - start_time), 2)) + " seconds ---")
+    print("--- " + str(round((time.time() - start_time), 2)) + " seconds ---")
     return
+
 
 if __name__ == "__main__":
     main(sys.argv)
--- main.py
+++ main.py
@@ -1,89 +1,63 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 21 16:40:56 2020
-
-@author: blobfishman
-# """
-
-
 import sys
 import geopandas as gpd
 import pandas as pd
-pd.set_option('display.max_rows', 20)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
 import numpy as np
 import time
 
+
 def main(args):
-    if len(args) != 5 :
+    if len(args) != 5:
         print("missing arguments. Usage:")
-        print("python3 main.pyINFRA_SCORES.shp POINTS.shp POINTS_BACKGROUND.shp SCORES_FINAL.shp")
+        print("python3 main.pyINFRA_SCORES.shp POINTS.shp"
+              "POINTS_BACKGROUND.shp SCORES_FINAL.shp")
         return
 
     start_time = time.time()
-
-    # Grid File no longer needed as it is also included in INFRA_SCORES File
-    # print("Loading Grid Shape File at " + args[1])
-    # try:
-    #     grid = gpd.read_file(args[1])
-    #     try:
-    #         del grid['left']
-    #         del grid['top']
-    #         del grid['bottom']
-    #         del grid['right']
-    #     except:
-    #         pass
-    # except:
-    #     print("Grid Shape File could not be located or is in the wrong format. Do you also have the .shx and .dbf files in the same directory?. Aborting..")
-    #     return
 
     print("Loading Infra Scores Shape File at " + args[1])
 
     try:
         infra = gpd.read_file(args[1])
-        grid  = gpd.read_file(args[1])
-        del grid["infra_scor"]
+        infra = infra.drop(infra.columns.difference(['id', 'infra_scor',
+                                                     'geometry']), 1)
+        grid = gpd.read_file(args[1])
+        grid = grid.drop(grid.columns.difference(['id', 'geometry']), 1)
     except:
-        print("Points Infra Scores Shape File could not be located or is in the wrong format. Do you also have the .shx and .dbf files in the same directory?. Aborting..")
+        print("Points Infra Scores Shape File could not be located or is in"
+              " the wrong format. Do you also have the .shx and .dbf files in"
+              " the same directory?. Aborting..")
         return
 
     print("Loading Points Shape File at " + args[2])
 
     try:
         points = gpd.read_file(args[2])
-        try:
-            del points['field_1']
-            del points['field_2']
-        except:
-            pass
+        points = points.drop(points.columns.difference(['geometry']), 1)
     except:
-        print("Points Shape File could not be located or is in the wrong format. Do you also have the .shx and .dbf files in the same directory?. Aborting..")
+        print("Points Shape File could not be located or is in the wrong"
+              " format. Do you also have the .shx and .dbf files in the same"
+              " directory?. Aborting..")
         return
 
     print("Loading Points Background Shape File at " + args[3])
 
     try:
         noise_points = gpd.read_file(args[3])
-        try:
-            del points['field_1']
-            del points['field_2']
-        except:
-            pass
+        noise_points = noise_points.drop(noise_points.columns.difference(
+                        ['geometry']), 1)
     except:
-        print("Points Background Shape File could not be located or is in the wrong format. Do you also have the .shx and .dbf files in the same directory?. Aborting..")
+        print("Points Background Shape File could not be located or is in the"
+              " wrong format. Do you also have the .shx and .dbf files in the"
+              " same directory?. Aborting..")
         return
 
-    #points = gpd.read_file(args[2])
-
-    #print("points")
-    #print(points)
+    # print("points")
+    # print(points)
     print("Calculating final scores...")
     # join points
     dfsjoin = gpd.sjoin(grid, points, how="left", op='contains')
-    #print("join")
-    #print(dfsjoin)
+    # print("join")
+    # print(dfsjoin)
 
     # Super ekliges Workaround:
     # Zählfunktion ignoritert Geometry und nan
@@ -92,62 +66,65 @@ def main(args):
         ct = 0
         for l in lst:
             if str(type(l)) == "<class 'float'>":
-                if  not str(l) == "nan":
-                    #print(l)
-                    ct+=1
+                if not str(l) == "nan":
+                    ct += 1
         return ct
     # dropna macht nichts?
-    dfpivot = pd.pivot_table(dfsjoin,index='id', aggfunc={'index_right':nanlen}, dropna=True)
-    #print("pivot")
-    #print(dfpivot)
+    dfpivot = pd.pivot_table(dfsjoin, index='id',
+                             aggfunc={'index_right': nanlen}, dropna=True)
+    # print("pivot")
+    # print(dfpivot)
 
-    dfmerge = grid.merge(dfpivot, how='left',on='id')
-    #print("merge")
-    #print(dfmerge)
+    dfmerge = grid.merge(dfpivot, how='left', on='id')
+    # print("merge")
+    # print(dfmerge)
 
-    # load infrastructure
-    #infra = gpd.read_file(args[3])
-    #print("infra")
-    #print(infra)
-    # join noise points
-    #noise_points = gpd.read_file(args[4])
+    # Infrastructure
+    # print("infra")
+    # print(infra)
+    # Noise Points
 
     dfsjoin_noise = gpd.sjoin(grid, noise_points, how="left", op='contains')
-    dfpivot_noise = pd.pivot_table(dfsjoin_noise,index='id', aggfunc={'index_right':nanlen})
+    dfpivot_noise = pd.pivot_table(dfsjoin_noise, index='id',
+                                   aggfunc={'index_right': nanlen})
 
-    dfmerge_noise = grid.merge(dfpivot_noise, how='left',on='id')
-    #print("merge noise")
-    #print(dfmerge_noise)
+    dfmerge_noise = grid.merge(dfpivot_noise, how='left', on='id')
+    # print("merge noise")
+    # print(dfmerge_noise)
 
     # # calculate value
     val_points = dfmerge["index_right"]
     val_points_noise = dfmerge_noise["index_right"]
-    y  = infra["infra_scor"]
+    y = infra["infra_scor"]
 
-    #Grundlage
+    # Grundlage
     '''x = np.maximum(val_points-val_points_noise, np.ones(len(y)))
     # normalisieren
     val = x**y + x # Funktion'''
 
-    #Normalisieren
+    # Normalisieren
 
-    #Problem: Exponentielle Ansteckungsgefahr bei lin. Wachstum der Menschenanzahl wird eliminiert.
+    # Problem: Exponentielle Ansteckungsgefahr bei
+    #          lin. Wachstum der Menschenanzahl wird eliminiert.
     '''x = np.maximum(val_points-val_points_noise, np.ones(len(y)))
     x = np.log(x) + 1
     val = x**y + x # Funktion'''
 
-    #Idee/Ansatz: Kleinere Basis als exp(x) um exponentielles Wachstum zu erhalten.
+    # Idee/Ansatz: Kleinere Basis als exp(x) um exponentielles
+    #              Wachstum zu erhalten.
     x = np.maximum(val_points-val_points_noise, np.ones(len(y)))
     ''' Wurzel entfernt: x = 2**np.log(x)
     Warum sollten wir erhöhte Menschenaufkommen
     bestrafen? Genau darum geht es doch, oder?'''
     val = x**y
 
-    '''Problem/Ansatz: Keine Unterscheidung zwischen guten und irrelevanten Aufkommen
-     x = np.maximum(val_points-val_points_noise, np.ones(len(y)))
+    '''Problem/Ansatz: Keine Unterscheidung zwischen guten und
+                       irrelevanten Aufkommen
+    x = np.maximum(val_points-val_points_noise, np.ones(len(y)))
     val = x**y'''
 
-    '''Idee: Vertausche Argumente -> gute Infrastrukturen werden von irrelevanten unterschieden.
+    '''Idee: Vertausche Argumente -> gute Infrastrukturen werden von
+             irrelevanten unterschieden.
     x = np.maximum(val_points-val_points_noise, np.ones(len(y)))
     x = x / np.max(x) + 1
     val = y**x'''
@@ -155,7 +132,7 @@ def main(args):
     # create return file
     dffinal = dfmerge_noise
     dffinal["index_right"] = val
-    dffinal.columns=["id", "geometry", "val"]
+    dffinal.columns = ["id", "geometry", "val"]
     dffinal['pop'] = x
     dffinal['infra_score'] = y
     print("Resulting Grid:")
@@ -171,8 +148,9 @@ def main(args):
         print("Unsupported Export File format:" + args[4] + ". Aborting....")
         return
     print("Finished")
-    print("--- "+ str(round((time.time() - start_time), 2)) + " seconds ---")
+    print("--- " + str(round((time.time() - start_time), 2)) + " seconds ---")
     return
-    
+
+
 if __name__ == "__main__":
     main(sys.argv)
